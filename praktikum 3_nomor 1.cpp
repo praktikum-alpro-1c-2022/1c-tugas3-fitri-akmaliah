db974663d7be761d122015811eb02297fa202887
Fitriakmaliah <114641149+Fitriakmaliah@users.noreply.github.com>
2022-10-20 15:44:01 +0700

    Tugas Praktikum 3
    saya telah mengerjakan tugas praktikum 3

---
+++ praktikum 3_nomor 1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main () {
+    float alas = 8, tinggi = 10;
+    float SisiMiring;
+
+    SisiMiring = sqrt ( pow (alas,2) + pow (tinggi,2));
+
+    cout <<"Nilai Sisi Miring = " << SisiMiring << endl;
+
+return 0;
+}
---
+++ praktikum 3_nomor 2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main () {
+    float praktikum = 75, uts = 70, uas = 88;
+    float NilaiAkhir;
+
+    cout << "Nilai Praktikum = " << praktikum << endl;
+    cout << "Nilai uts = " << uts << endl;
+    cout << "Nilai uas = " << uas << endl;
+
+    NilaiAkhir = (praktikum * 20/100) + (uts * 30/100) + (uas * 50/100);
+    cout << "Nilai akhirnya adalah = " << NilaiAkhir << endl;
+
+return 0;
+}
